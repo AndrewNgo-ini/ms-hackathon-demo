3ac1339ff3ee4f24539396799bbdfe6e1e8ae5ac
Hieu Ngo <46411807+AndrewNgo-ini@users.noreply.github.com>
2024-06-21 10:36:29 +0700

    update azure (#8)

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
+import os 
+
 from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI, AzureChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
 
 def get_agent():
-    llm = OpenAI(temperature=0)
+    #llm = OpenAI(temperature=0)
+    llm = AzureChatOpenAI(
+        openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+        azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+    )
 
     tools = load_tools(["serpapi"], llm=llm)
     agent = initialize_agent(
